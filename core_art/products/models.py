2c4ceba0a906d43c79203a6c9869dbb7d4743b90
Kiarash <shahedikiarash@gmail.com>
2024-12-16 14:19:01 +0330

    initial commit > making project with product and users and orders app

---
+++ core_art/products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from users import User
+
+class Product(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    categories = models.ManyToManyField(Category)
+    images = models.ManyToManyField(ProductImage)
+    date_added = models.DateTimeField(auto_now_add=True)
